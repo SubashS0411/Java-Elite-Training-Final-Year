bb6711f23ac55a2027d965151323ab6354b98569
subashs0411 <subashs2573@gmail.com>
2026-07-06 07:17:44 +0530

    Solved subset problems

---
+++ StringProblems/src/Subset.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subset {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+        System.out.println(subsets(arr));
+
+    }
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        for(int num:nums){
+            int n=outer.size();
+            for(int i=0;i<n;i++){
+                List<Integer> inner=new ArrayList<>(outer.get(i));
+                inner.add(num);
+                outer.add(inner);
+            }
+        }
+        return outer;
+    }
+}
