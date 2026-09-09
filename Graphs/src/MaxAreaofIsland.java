72a7b809dced8fbeafe20196c581b56b22e50bbe
subashs0411 <subashs2573@gmail.com>
2026-09-09 13:59:57 +0530

    Solved the problem MaxAreaOfIsland

---
+++ Graphs/src/MaxAreaofIsland.java
@@ -0,0 +1,32 @@
+public class MaxAreaofIsland {
+    private static final int[][] directions={{1,0},{-1,0},{0,1},{0,-1}};
+    public static int maxAreaOfIsland(int[][] grid) {
+        if(grid==null|| grid.length==0) return 0;
+        int maxArea=0,row=grid.length,col= grid[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if(grid[i][j]==1){
+                    int current=dfsArea(grid,i,j);
+                    maxArea=Math.max(current,maxArea);
+                }
+            }
+        }
+        return maxArea;
+    }
+    public static int dfsArea(int[][] grid,int row,int col){
+        if(row<0||col<0||row>= grid.length||col>= grid[0].length||grid[row][col]==0){
+            return 0;
+        }
+        grid[row][col]=0;
+        int area=1;
+        for (int[] dir:directions){
+            area+=dfsArea(grid,row+dir[0],col+dir[1]);
+        }
+        return area;
+    }
+
+    public static void main(String[] args) {
+        int[][] grid={{0,1,1,0,1},{1,0,1,0,1},{0,1,1,0,1},{0,1,0,0,1}};
+        System.out.println(maxAreaOfIsland(grid));
+    }
+}
