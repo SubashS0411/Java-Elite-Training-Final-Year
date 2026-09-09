ef076c7c582184eb63c24ad6b4f173fd35c0566b
subashs0411 <subashs2573@gmail.com>
2026-09-09 07:32:01 +0530

    Solved the problem numberofislands

---
+++ Graphs/src/NumberofIslands.java
@@ -0,0 +1,36 @@
+public class NumberofIslands {
+    public static int numIsLands(char[][] grid){
+        int row=grid.length;
+        int cols= grid[0].length;
+        int numofisland=0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < cols; j++) {
+                if(grid[i][j]=='1'){
+                    numofisland++;
+                    sinkdfs(grid,i,j);
+                }
+            }
+        }
+        return numofisland;
+    }
+    public static void sinkdfs(char[][]grid,int row,int col){
+        if(row<0||col<0||row>= grid.length||col>= grid[0].length||grid[row][col]=='0'){
+            return;
+        }
+        grid[row][col]='0';
+        sinkdfs(grid, row-1, col);
+        sinkdfs(grid, row+1, col);
+        sinkdfs(grid, row, col-1);
+        sinkdfs(grid, row, col+1);
+    }
+
+    public static void main(String[] args) {
+        char[][] grid1 = {
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+        System.out.println(numIsLands(grid1));
+    }
+}
