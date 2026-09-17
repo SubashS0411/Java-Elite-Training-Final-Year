c7d50a26dc8512436226b4b7416b377cc5d4cb3d
subashs0411 <subashs2573@gmail.com>
2026-09-17 07:29:25 +0530

    Solved the leetcode problem Min Cost Climbing Stairs

---
+++ 1-D Dynamic Programming/src/MinCostClimbingStairs.java
@@ -0,0 +1,19 @@
+public class MinCostClimbingStairs {
+    public static int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        int[] dp=new int[n+1];
+        dp[0]=0;
+        dp[1]=0;
+        for (int i = 2; i <=n ; i++) {
+            int onestep=dp[i-1]+cost[i-1];
+            int twostep=dp[i-2]+cost[i-2];
+            dp[i]=Math.min(onestep,twostep);
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minCostClimbingStairs(new int[]{1,2,3}));
+        System.out.println(minCostClimbingStairs(new int[]{1,2,1,2,1,1,1}));
+    }
+}
